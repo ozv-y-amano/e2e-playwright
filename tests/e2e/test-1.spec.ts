e65da75df1dd3f13263745acbbbf830075c361f1
ozv-y-amano <y-amano@oz-vision.co.jp>
2024-08-26 18:53:21 +0900

    add: GitHub Actions

--- tests/e2e/test-1.spec.ts
+++ tests/e2e/test-1.spec.ts
@@ -1,6 +1,15 @@
 import { test, expect } from "@playwright/test";
 
 test("test", async ({ page }) => {
+	// サーバーの起動確認を追加
+	await page.waitForFunction(
+		() =>
+			fetch("http://localhost:3000/todos")
+				.then((res) => res.ok)
+				.catch(() => false),
+		{ timeout: 30000 }
+	);
+
 	await page.goto("http://localhost:3000/todos");
 	await page.getByPlaceholder("Add a new task").click();
 	await page.getByPlaceholder("Add a new task").fill("新しいタスク");
